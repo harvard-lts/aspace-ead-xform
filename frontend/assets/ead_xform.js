49bff0e8c238132448ebcfdd451400422796629f
Dave Mayo <dave_mayo@harvard.edu>
2026-01-12 10:54:04 -0500

    Missed js change

--- frontend/assets/ead_xform.js
+++ frontend/assets/ead_xform.js
@@ -1,6 +1,6 @@
 /* any js needed for EAD transforms */
 $(function () {
-    var csvBtnAdded = (typeof  ead_xform_id === 'undefined'); /* if we don't have the id to beginwith, forget it*/    
+    var csvBtnAdded = (typeof  ead_xform_id === 'undefined'); /* if we don't have the id to beginwith, forget it*/
 	/* the defer methodolgy is adapted from https://stackoverflow.com/questions/7486309/how-to-make-script-execution-wait-until-jquery-is-loaded */
 	/* since the lower half of the screen loads *after* the javascript is loaded, we need to to this */
     function defer(method) {
@@ -17,7 +17,7 @@ $(function () {
 			var $eadBtn = $("#download-ead-dropdown");
 			if ($eadBtn.length > 0) {
 				a = '/resources/' +  ead_xform_id + '/staff_csv';
-				$eadBtn.after('<li class="hvd-download-csv"><a href="' + a + '">Download CSV</a></li>');
+				$eadBtn.after('<li class="hvd-download-csv"><a class="dropdown-item" ref="' + a + '">Download CSV</a></li>');
 				csvBtnAdded = true;
 			}
 		}
